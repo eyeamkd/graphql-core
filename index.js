8b49f52b380fc5a24d84fc3925e8a1c957e9f3df
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-12 13:57:21 +0530

    refactored the file using .graphql schema

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const {ApolloServer} = require('apollo-server');   
-const {typeDefs,resolvers} = require('./schema/index');
+const { ApolloServer } = require("apollo-server");
+// const { resolvers } = require("./schema/index");
+const path = require("path");
+const fs = require("fs");
+
+const resolvers = {
+  Query: {
+    info: () => null,
+    //used to resolve the feed query that was placed
+    feed: () => links,
+  },
+  Mutation: {
+    newPost: (parent, context) => ({
+      id,
+      url: context.url,
+      description: context.description,
+    }),
+  },
+};
+
 
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers, 
-}) 
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, './schema/schema.graphql'),
+    "utf8"
+  ),
+  resolvers,
+});
 
-server.listen().then(({url})=>console.log("Server listening at ",url));
+server.listen().then(({ url }) => console.log("Server listening at ", url));
--- schema/index.js
+++ schema/index.js
@@ -1,36 +1,25 @@
-const { gql } = require("apollo-server-core");
+const { gql } = require("apollo-server-core"); 
+let links = [
+  {
+    id: "1234",
+    url: "www.kunaldubey.com",
+    description: "This is the website of Kunal Dubey",
+  },
+];
 
-const typeDefs = gql`  
-type Query{  
-    info:String!
-    feed:[Link!]
-}, 
+const resolvers = {
+  Query: {
+    info: () => null,
+    //used to resolve the feed query that was placed
+    feed: () => links,
+  }, 
+  Mutation:{
+      newPost:(parent,context)=>({
+          id,
+          url:context.url,
+          description:context.description
+      })
+  }
+};
 
-type Link{
-    url:String!,
-    id:ID!,
-    description:String!
-}
-`  
-
-let links = [{ 
-    id:'1234',
-    url:'www.kunaldubey.com',
-    description:'This is the website of Kunal Dubey'
-}]
-
-const resolvers = { 
-    Query:{
-        info:()=>'This is an GraphQL API for HackerNews clone', 
-        //used to resolve the feed query that was placed
-        feed:()=>links,
-    }, 
-    Link:{
-        id:(parent)=> parent.id, 
-        url:(parent)=> parent.url,
-        description:(parent)=> parent.description
-    }
-
-} 
-
-module.exports = {typeDefs,resolvers}
+module.exports = { resolvers };
