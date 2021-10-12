e2f273fa8b939dbd4d5779e0b61ab82cb03afc9b
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-12 18:04:39 +0530

    able to persist data on SQLite DB

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { ApolloServer } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
 const { resolvers, links } = require("./schema/index");
 const path = require("path");
 const fs = require("fs");
+
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(
@@ -9,6 +12,10 @@ const server = new ApolloServer({
     "utf8"
   ),
   resolvers,
+  //context object is just a passon object, that multiple resolvers can read and write to
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log("Server listening at ", url));
--- schema/index.js
+++ schema/index.js
@@ -1,47 +1,42 @@
-const { gql } = require("apollo-server-core"); 
-let links = [
-  {
-    id: "1234",
-    url: "www.kunaldubey.com",
-    description: "This is the website of Kunal Dubey",
-  },
-];
+const { gql } = require("apollo-server-core");
 
 const resolvers = {
-    Query: {
-      info: () => "GraphQL query for the HackerNews API",
-      //used to resolve the feed query that was placed
-      feed: () => links,
-    },
-    Mutation: {
-      newLink: (parent, arguments) => {  
-          let len = links.length + 1;
-          let link = {
-            id:`link-${len}-${Date.now()}`,
-            url: arguments.url,
-            description: arguments.description,
-          }
-        links.push(link); 
-        return link
-      },
-      updateLink:(parent,arguments)=>{
-        let resultIndex;
-        let result = links.find((link,index)=> { 
-           if(link.id==arguments.id){
-               resultIndex = index;
-               return true;
-           }
-        });
-        let link = {id:arguments.id,url:arguments.url,description:arguments.description}; 
-        links[resultIndex] = link; 
-        return `Link ${result.id} successfully updated`
-      },
-      deleteLink:(parent, arguments)=>{
-        let newLinks = links.filter(link=> link.id!=arguments.id) 
-        links = newLinks;
-        return `Link ${arguments.id} successfully deleted`
-      }
-    },
-  };
+  Query: {
+    info: () => "GraphQL query for the HackerNews API",
+    //used to resolve the feed query that was placed
+    feed: async (parent, arguments, context) => context.prisma.link.findMany(),
+  },
+  Mutation: {
 
-module.exports = { resolvers,links };
+    newLink: (parent, arguments, context, info) => {
+      const newLink = context.prisma.link.create({
+        data:{
+          url: arguments.url,
+          description: arguments.description,
+        }
+      }); 
+      return newLink;
+    },
+
+    updateLink: (parent, arguments, context, info) => {
+      let resultIndex;
+      let result = links.find((link, index) => {
+        if (link.id == arguments.id) {
+          resultIndex = index;
+          return true;
+        }
+      });
+      let link = { id: arguments.id, url: arguments.url, description: arguments.description };
+      links[resultIndex] = link;
+      return `Link ${result.id} successfully updated`
+    },
+
+    deleteLink: (parent, arguments, context, info) => {
+      let newLinks = links.filter(link => link.id != arguments.id)
+      links = newLinks;
+      return `Link ${arguments.id} successfully deleted`
+    }
+  },
+};
+
+module.exports = { resolvers };
