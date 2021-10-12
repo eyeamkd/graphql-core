9e6e28796e2eef6da0da7f7ec2e1e4816cf5aeb2
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-12 16:08:54 +0530

    CRUD Operations WIP

--- schema/index.js
+++ schema/index.js
@@ -14,15 +14,33 @@ const resolvers = {
       feed: () => links,
     },
     Mutation: {
-      newPost: (parent, arguments) => { 
-        let link = { 
-        id:`link-${links.length++}-${Date.now()}`,
-        url: arguments.url,
-        description: arguments.description,
-        } 
+      newLink: (parent, arguments) => {  
+          let len = links.length + 1;
+          let link = {
+            id:`link-${len}-${Date.now()}`,
+            url: arguments.url,
+            description: arguments.description,
+          }
         links.push(link); 
-        return link;
+        return link
       },
+      updateLink:(parent,arguments)=>{
+        let resultIndex;
+        let result = links.find((link,index)=> { 
+           if(link.id==arguments.id){
+               resultIndex = index;
+               return true;
+           }
+        });
+        let link = {id:arguments.id,url:arguments.url,description:arguments.description}; 
+        links[resultIndex] = link; 
+        return `Link ${result.id} successfully updated`
+      },
+      deleteLink:(parent, arguments)=>{
+        let newLinks = links.filter(link=> link.id==arguments.id) 
+        links = newLinks;
+        return `Link ${arguments.id} successfully deleted`
+      }
     },
   };
 
