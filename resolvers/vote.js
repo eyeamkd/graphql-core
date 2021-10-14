5cb14959c88d48cc7a657369bf88d8a9ec073fac
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-14 17:27:31 +0530

    Subs for voting working

--- resolvers/vote.js
+++ resolvers/vote.js
@@ -1,12 +1,12 @@
 const prisma = require('../utils/prisma');
 
 function link(parent) {
-  return prisma.vote.findUnique({ where: { id: parent.id } });
+  return prisma.vote.findUnique({ where: { id: parent.id } }).link();
 }
 
 function user(parent) { 
     console.log("vote user",prisma.vote);
-  return prisma.vote.findUnique({ where: { id: parent.id } });
+  return prisma.vote.findUnique({ where: { id: parent.id } }).user();
 }
 
 module.exports = {
