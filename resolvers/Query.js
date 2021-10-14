5f32f39e13ad07d061ba51150709c2c1e6f32dc1
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-14 17:56:06 +0530

    implemented queries

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,5 +1,20 @@
-function feed(parent, arguments, context, info){ 
-    return context.prisma.link.findMany(); 
-} 
+const prisma = require('../utils/prisma');
+
+function feed(parent, arguments, context, info){  
+    const {filter} = arguments;  
+    let query = ''; 
+    if(filter) 
+        return prisma.link.findMany({
+            where:{ 
+                OR:[ 
+                    {url:{contains:filter} }, 
+                    {description:{contains:filter}}
+
+                ]  
+            } 
+        }) 
+    else
+    return prisma.link.findMany({}); 
+        }  
 
 module.exports = {feed};
